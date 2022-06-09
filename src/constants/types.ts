6c61015374e9a85682952ab3933e820375da9c9e
patipat <lpatipat@icloud.com>
2022-06-08 21:17:10 -0400

    update config

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -1,6 +1,10 @@
+import { ethers } from "ethers";
+import { chain } from "wagmi";
+
+type networks = keyof typeof chain
 export interface Parameters {
   CHAIN_ID: string;
-  NETWORK_NAME: string;
+  NETWORK_NAME: networks;
   LOTTERY_ADDRESS: string;
   AUCTION_ADDRESS: string;
   REWARDS_ADDRESS: string;
