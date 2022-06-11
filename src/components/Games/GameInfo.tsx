f5421838304450224fb8ec699f09a5eba78077c6
patipat <lpatipat@icloud.com>
2022-06-10 22:36:40 -0400

    feat: bid history tab in game info

--- src/components/Games/GameInfo.tsx
+++ src/components/Games/GameInfo.tsx
@@ -5,33 +5,37 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import useTabs from '@/hooks/useTabs';
 import type { Auction } from '@prisma/client';
 import { gql, useQuery } from '@apollo/client';
-import shortenAddress from '@/utilities/shortenAddress';
+import { ethers } from 'ethers';
 
 interface Props {
   drop: Drop;
   auction?: Auction;
 }
 
+function formatAmount(amount: any) {
+  const amountString = String(amount);
+  const amountBigNumber = ethers.BigNumber.from(amountString);
+
+  const amountFormatted = ethers.utils.formatUnits(amountBigNumber);
+  const amountFormattedShortened = parseFloat(amountFormatted).toFixed(6);
+  return { amountFormatted, amountFormattedShortened };
+}
+
+interface BidHistoryItem {
+  bidder: string;
+  amount: number;
+}
+
+function sortBidHistory(bidHistory: BidHistoryItem[]) {
+  const bidHistoryCopy = [...bidHistory];
+  const sorted = bidHistoryCopy.sort((a, b) => +b.amount - +a.amount);
+  return sorted;
+}
+
 // styles/layout/_game-page.scss
 export default function GameInfo({ drop, auction }: Props) {
   const router = useRouter();
   const { handleTabsClick, selectedTabIndex } = useTabs();
-
-  const BID_HISTORY_QUERY = gql`
-    query GetBidHistory($auctionId: String) {
-      auction(id: $auctionId) {
-        bids {
-          bidder
-          amount
-        }
-      }
-    }
-  `;
-  const auctionIdHexStr = '0x' + auction?.id.toString(16);
-  const { data } = useQuery(BID_HISTORY_QUERY, {
-    variables: { auctionId: auctionIdHexStr },
-  });
-
   return (
     <Tabs className='game-info' selectedIndex={selectedTabIndex} onSelect={handleTabsClick}>
       <TabList className='game-info__tab-list'>
@@ -50,28 +54,7 @@ export default function GameInfo({ drop, auction }: Props) {
             className='game-info__tab-panel'
             selectedClassName='game-info__tab-panel--active'
           >
-            <table className='game-info__bid-history-table'>
-              <tr className='game-info__bid-history-header'>
-                <th>TIME</th>
-                <th>USER</th>
-                <th>AMOUNT</th>
-              </tr>
-
-              {data &&
-                data.auction &&
-                data.auction.bids.map(({ bidder, amount }, i: number) => (
-                  // <div key={i}>
-                  //   <p>
-                  //     {shortenAddress(bidder)} -&gt; {amount}
-                  //   </p>
-                  // </div>
-                  <tr className='game-info__bid-history-item' key={i}>
-                    <th>time</th>
-                    <th>{bidder}</th>
-                    <th>{amount}</th>
-                  </tr>
-                ))}
-            </table>
+            <BidHistoryTable auctionId={auction.id} />
           </TabPanel>
         )}
         <TabPanel className='game-info__tab-panel' selectedClassName='game-info__tab-panel--active'>
@@ -88,5 +71,58 @@ export default function GameInfo({ drop, auction }: Props) {
         </TabPanel>
       </div>
     </Tabs>
+  );
+}
+
+interface BidHistoryTableProps {
+  auctionId: number;
+}
+
+function BidHistoryTable({ auctionId }: BidHistoryTableProps) {
+  const BID_HISTORY_QUERY = gql`
+    query GetBidHistory($auctionId: String) {
+      auction(id: $auctionId) {
+        bids {
+          bidder
+          amount
+        }
+      }
+    }
+  `;
+  const auctionIdHexStr = '0x' + auctionId.toString(16);
+  const {
+    data: { auction: { bids } } = { auction: { bids: [] } },
+    loading: isLoading,
+    startPolling,
+  } = useQuery(BID_HISTORY_QUERY, {
+    variables: { auctionId: auctionIdHexStr },
+  });
+  // if (bidHistoryQueryLoading || !data || !data.auction || !data.auction.bids) return null;
+  const sortedBids = sortBidHistory(bids);
+  startPolling(500);
+
+  return (
+    <table className='game-info__bid-history-table'>
+      <thead>
+        <tr className='game-info__bid-history-header'>
+          <th data-col='time'>TIME</th>
+          <th data-col='bidder'>USER</th>
+          <th data-col='amount'>AMOUNT</th>
+        </tr>
+      </thead>
+      <tbody className='game-info__bid-history-body'>
+        {sortedBids.map(({ bidder, amount }) => {
+          const { amountFormatted, amountFormattedShortened } = formatAmount(amount);
+          if (isLoading) return null;
+          return (
+            <tr className='game-info__bid-history-item' key={amountFormatted}>
+              <th data-col='time'>time</th>
+              <th data-col='bidder'>{bidder}</th>
+              <th data-col='amount'>{amountFormattedShortened}</th>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
