e4ff959072ab826be70d52a9fe0cbfccb611412c
spider-g <guilherme@sage.art>
2022-08-17 18:57:12 -0300

    fix: build error

--- src/pages/api/nfts.ts
+++ src/pages/api/nfts.ts
@@ -98,7 +98,7 @@ async function createOffer(request: NextApiRequest, response: NextApiResponse) {
 }
 
 async function deleteOffer(request: NextApiRequest, response: NextApiResponse) {
-  const id = +request.query.id;
+  const id = Number(request.query.id);
   const session = await getSession({ req: request });
   const address: string = session?.address as string;
   const state = OfferState.ACTIVE;
