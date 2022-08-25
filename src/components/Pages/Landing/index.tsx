6483ba29ae47c29bcb325684c0572acb55b4432b
patipat <lpatipat@icloud.com>
2022-08-25 15:02:15 -0400

    css tweak: landing page

--- src/components/Pages/Landing/index.tsx
+++ src/components/Pages/Landing/index.tsx
@@ -6,10 +6,13 @@ export default function LandingPage() {
         <div className='landing-page__sage-logo'>
           <SageFullLogoSVG className='landing-page__sage-svg' />
         </div>
-        <h1 className='landing-page__title'>
-          SAGE is a curation system created to refine and lead Web3. <pre /> Through our portal, you
-          secure an entrance into the future.
-        </h1>
+        <div>
+          <h1 className='landing-page__title'>
+            SAGE is a curation system created to refine and lead Web3. <pre /> Through our portal,
+            you secure an entrance into the future.
+            <span className='landing-page__contact-label'>contact@sage.art</span>
+          </h1>
+        </div>
       </div>
     </div>
   );
