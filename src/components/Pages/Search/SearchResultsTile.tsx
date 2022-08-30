1ecf159cd6d7880e82d3ceb05adc3fac00db2265
patipat <lpatipat@icloud.com>
2022-08-30 10:01:38 -0400

    style: update search results display

--- src/components/Pages/Search/SearchResultsTile.tsx
+++ src/components/Pages/Search/SearchResultsTile.tsx
@@ -11,10 +11,6 @@ type ImgAspectRatio = '16/9' | '9/16' | '1/1';
 
 function getRandomizedAspectRatio(i: number): ImgAspectRatio {
   let dataAspect: ImgAspectRatio = '1/1';
-  if (i % 2 === 0) {
-    dataAspect = '16/9';
-  }
-
   if (i % 3 === 0) {
     if (i !== 6) {
       dataAspect = '9/16';
@@ -22,20 +18,10 @@ function getRandomizedAspectRatio(i: number): ImgAspectRatio {
   }
   if (i % 5 === 0) {
     if (i !== 10) {
-      dataAspect === '1/1';
+      dataAspect = '16/9';
     }
   }
-
-  const rand = Math.random();
-  if (rand < 0.33) {
-    return '16/9';
-  }
-
-  if (rand > 0.66) {
-    return '9/16';
-  }
-
-  return '1/1';
+  return dataAspect;
 }
 
 export default function SearchResultsTile({ nft, i }: Props) {
@@ -47,17 +33,15 @@ export default function SearchResultsTile({ nft, i }: Props) {
       router.push(`/artists/${nft.artist}`);
     }
   };
-
   const dataAspect = getRandomizedAspectRatio(i);
-
   return (
     <div className='search-page__grid-item' data-aspect={dataAspect} onClick={handleClick}>
       <div className='search-page__grid-item-img'>
         <BaseMedia src={nft.s3Path}></BaseMedia>
       </div>
       <div className='search-page__grid-item-info'>
-        <h1 className='search-page__grid-item-info-drop-name'>by {nft.artist}</h1>
-        <h1 className='search-page__grid-item-info-game-name'>{nft.name}</h1>
+        <h1 className='search-page__grid-item-info-nft-name'>{nft.name}</h1>
+        <h1 className='search-page__grid-item-info-artist-name'>by {nft.artist}</h1>
       </div>
     </div>
   );
