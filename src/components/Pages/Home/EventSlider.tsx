86d27a828e89500bb9fa3d67dd1886e81e1018f0
patipat <lpatipat@icloud.com>
2022-09-01 00:51:08 -0400

    remove reformat date on event slider

--- src/components/Pages/Home/EventSlider.tsx
+++ src/components/Pages/Home/EventSlider.tsx
@@ -37,8 +37,30 @@ export default function EventSlider({ mediumData }: Props) {
   return (
     <div className='home-page__events'>
       {state.items.map((item, i: number) => {
+        // const date = reformatDate(item.pubDate);
         return (
           <a target='__blank' key={i} className='home-page__event-slide' href={item.link}>
+            <h1 className='home-page__event-slide-header'>news</h1>
+            <Image
+              src={item.thumbnail}
+              layout='fill'
+              objectFit='cover'
+              loader={({ src, width, quality }) => {
+                return `${src}?w=${width}&q=${quality || 75}`;
+              }}
+            />
+            <div className='home-page__event-slide-focus' />
+            <div className='home-page__event-slide-content'>
+              <h1 className='home-page__event-slide-content-title'>{item.title}</h1>
+              <div className='home-page__event-slide-content-group'>
+                <p className='home-page__event-slide-content-description'>
+                 date 
+                </p>
+                <button className='home-page__event-slide-content-read-more-button'>
+                  read more
+                </button>
+              </div>
+            </div>
           </a>
         );
       })}
