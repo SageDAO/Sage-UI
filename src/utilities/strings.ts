0ebb0ac4fac8ecd2983ac9abddaad16f6948e830
patipat <lpatipat@icloud.com>
2022-09-01 00:59:23 -0400

    fix reformat date

--- src/utilities/strings.ts
+++ src/utilities/strings.ts
@@ -1,5 +1,6 @@
 export function reformatDate(date: string | Date | number) {
-  const reformatted = new Date(date).toISOString();
-// .split('T')[0].replaceAll('-', '.')
+  const dateObj = new Date(date);
+  const reformatted = dateObj.toISOString();
+  // .split('T')[0].replaceAll('-', '.')
   return reformatted;
 }
