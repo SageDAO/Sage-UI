6987aa66e67e3f4f34c71022e1d4b486f291970b
Patipat <patipat@sage.art>
2022-09-13 10:28:24 -0400

    feat: Sage Press Page

--- src/pages/press.tsx
+++ src/pages/press.tsx
@@ -5,6 +5,9 @@ import LogotypeWhite from '@/public/press/logotype-white.svg';
 import LogotypeBlack from '@/public/press/logotype-black.svg';
 
 export default function press() {
+  function handleDownloadSVGFiles() {
+    window.open('/press/logotype-white.svg');
+  }
   return (
     <div className='press-page'>
       <Logotype></Logotype>
@@ -14,9 +17,9 @@ export default function press() {
         </h1>
         <div className='press-page__assets-container'>
           <div className='press-page__icons-container'>
-            <div className='press-page__icon-white'>
+            <a className='press-page__icon-white'>
               <IconWhite />
-            </div>
+            </a>
             <div className='press-page__icon-black'>
               <IconBlack />
             </div>
@@ -32,9 +35,17 @@ export default function press() {
         </div>
         <h3 className='press-page__subheader'>sage logotype and icon</h3>
         <p className='press-page__subheader-info'>
-          Use this logotypes for any press releases or articles that is about SAGE. Please note SAGE
-          is trademarked logo and using this logotype is not permitted  outside press releases
+          Use this logotypes for any press releases or articles that is about SAGE. Please note SAGE
+          is trademarked logo and using this logotype is not permitted outside press releases
         </p>
+        <p className='press-page__subheader-info'>Sketch 92 - SVG downloadable</p>
+        <p className='press-page__subheader-info'>SVG Download</p>
+        <div className='press-page__downloads-container'>
+          <button className='press-page__download-button'>Download SKETCH Files</button>
+          <button onClick={handleDownloadSVGFiles} className='press-page__download-button'>
+            DOWNLOAD SVG Files
+          </button>
+        </div>
       </div>
     </div>
   );
