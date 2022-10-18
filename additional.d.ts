5f7ce27bff5270948c781db971440f9565cd00d2
Patipat <patipat@sage.art>
2022-10-17 23:08:55 -0400

    fix: module augmentation for SIWE and next-auth

--- additional.d.ts
+++ additional.d.ts
@@ -1,14 +1,10 @@
 import NextAuth, { DefaultSession } from 'next-auth';
 
-export declare module 'next-auth/react' {
+export declare module 'next-auth' {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user: {
-      /** The user's postal address. */
-      padd: string;
-    } & DefaultSession['user'];
     address: string;
   }
 }
