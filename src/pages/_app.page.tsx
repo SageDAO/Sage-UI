14620abeafe91d8f47d36e03ae3e01b967b21551
spider-g <guilherme@sage.art>
2022-10-20 15:54:04 -0300

    chore: remove provider for graphql, unused

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -8,13 +8,10 @@ import { SessionProvider } from 'next-auth/react';
 import store from '@/store/store';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout/Layout';
-import { createClient, WagmiConfig, chain, configureChains, defaultChains } from 'wagmi';
+import { createClient, WagmiConfig, configureChains, defaultChains } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { publicProvider } from 'wagmi/providers/public';
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { parameters } from '@/constants/config';
 import { useState } from 'react';
 import { SearchContext } from '@/store/searchContext';
 import LandingPage from '@/components/Pages/Landing';
@@ -26,6 +23,7 @@ const { chains, provider, webSocketProvider } = configureChains(defaultChains, [
   infuraProvider({ apiKey: process.env.INFURA_ID }),
   publicProvider(),
 ]);
+
 const connectors = [
   new InjectedConnector({
     chains,
@@ -45,12 +43,6 @@ const wagmiClient = createClient({
   webSocketProvider,
 });
 
-const { SUBGRAPH_URL } = parameters;
-const apolloClient = new ApolloClient({
-  uri: SUBGRAPH_URL,
-  cache: new InMemoryCache(),
-});
-
 function App({ Component, pageProps, router }: AppProps) {
   const { theme } = useTheme();
   const [query, setQuery] = useState<string | null>(null);
@@ -61,28 +53,26 @@ function App({ Component, pageProps, router }: AppProps) {
     <ReduxProvider store={store}>
       <WagmiConfig client={wagmiClient}>
         <SessionProvider refetchInterval={0}>
-          <ApolloProvider client={apolloClient}>
-            <SearchContext.Provider value={{ query, setQuery }}>
-              <Head>
-                <title>SAGE</title>
-                <link rel='icon' href='/icons/sage.svg' />
-                <meta charSet='utf-8' />
-                <meta
-                  name='viewport'
-                  content='width=device-width,initial-scale=1,viewport-fit=cover'
-                />
-                <meta name='theme-color' content={themeContent} />
-                <meta name='description' content='' />
-              </Head>
-              {isMaintenanceOn ? (
-                <LandingPage />
-              ) : (
-                <Layout router={router}>
-                  <Component {...pageProps} key={router.pathname} />
-                </Layout>
-              )}
-            </SearchContext.Provider>
-          </ApolloProvider>
+          <SearchContext.Provider value={{ query, setQuery }}>
+            <Head>
+              <title>SAGE</title>
+              <link rel='icon' href='/icons/sage.svg' />
+              <meta charSet='utf-8' />
+              <meta
+                name='viewport'
+                content='width=device-width,initial-scale=1,viewport-fit=cover'
+              />
+              <meta name='theme-color' content={themeContent} />
+              <meta name='description' content='' />
+            </Head>
+            {isMaintenanceOn ? (
+              <LandingPage />
+            ) : (
+              <Layout router={router}>
+                <Component {...pageProps} key={router.pathname} />
+              </Layout>
+            )}
+          </SearchContext.Provider>
         </SessionProvider>
       </WagmiConfig>
     </ReduxProvider>
