0a6391b19eaa00bb91e6f8efbb3ef31cefb2c1e8
spider-g <guilherme@sage.art>
2022-10-21 20:08:32 -0300

    feat: large video file on landing

--- src/components/LaunchTrailer.tsx
+++ src/components/LaunchTrailer.tsx
@@ -6,8 +6,8 @@ interface Props {
   onClick?: () => void;
 }
 
-const mobileViewSrc = 'https://d180qjjsfkqvjc.cloudfront.net/trailers/lehel_new.mp4';
-const desktopViewSrc = 'https://d180qjjsfkqvjc.cloudfront.net/trailers/lehel_new.mp4';
+const mobileViewSrc = 'https://d180qjjsfkqvjc.cloudfront.net/trailers/lehel.mp4';
+const desktopViewSrc = 'https://d180qjjsfkqvjc.cloudfront.net/trailers/lehel.mp4';
 
 function LaunchTrailer({ onClick }: Props) {
   const { isMobile } = useWindowDimensions();
