2c4f0bad8f2003f146ec9317722320465bd0b2d2
spider-g <guilherme@sage.art>
2022-10-21 14:58:07 -0300

    feat: new video url

--- src/components/LaunchTrailer.tsx
+++ src/components/LaunchTrailer.tsx
@@ -6,12 +6,11 @@ interface Props {
   onClick?: () => void;
 }
 
-const mobileViewSrc = 'https://s3.us-east-2.amazonaws.com/sage.art/trailers/drop1_mobile.mp4';
+const mobileViewSrc = 'https://d180qjjsfkqvjc.cloudfront.net/trailers/drop1_desktopfilegroup.mp4';
+const desktopViewSrc = 'https://d180qjjsfkqvjc.cloudfront.net/trailers/drop1_desktopfilegroup.mp4';
 
-const desktopViewSrc = 'https://s3.us-east-2.amazonaws.com/sage.art/trailers/drop1_desktop.mp4';
 function LaunchTrailer({ onClick }: Props) {
   const { isMobile } = useWindowDimensions();
-
   return <Hero bannerOnClick={onClick} imgSrc={isMobile ? mobileViewSrc : desktopViewSrc} />;
 }
 
