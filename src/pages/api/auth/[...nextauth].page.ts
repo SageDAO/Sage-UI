1b0487429590862e69bb747a9d577cd61f3fc46c
Patipat <patipat@sage.art>
2022-10-23 17:16:25 -0400

    WIP: twitter oath

--- src/pages/api/auth/[...nextauth].page.ts
+++ src/pages/api/auth/[...nextauth].page.ts
@@ -1,12 +1,22 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { getCsrfToken } from 'next-auth/react';
+import TwitterProvider from 'next-auth/providers/twitter';
 import { SiweMessage } from 'siwe';
 import { NextApiRequest, NextApiResponse } from 'next';
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   const providers = [
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+      version: '2.0',
+      authorization: {},
+      profile(profile) {
+        return null;
+      },
+    }),
     CredentialsProvider({
       name: 'Ethereum',
       credentials: {
