e2b1b095646a91b61de561e3715bdbda02403951
Patipat <patipat@sage.art>
2022-10-24 14:01:05 -0400

    update: tweak apply page text content

--- src/pages/submissions.page.tsx
+++ src/pages/submissions.page.tsx
@@ -13,25 +13,26 @@ export default function submissions() {
       <section className='submissions-page__main'>
         <div className='submissions-page__main-left'>
           <div className='submissions-page__guidelines-group'>
-            <h1 className='submissions-page__guidelines-header'>Submission Guidelines</h1>
+            <h1 className='submissions-page__guidelines-header'>Apply to SAGE</h1>
             <p className='submissions-page__guidelines-text'>
-              SAGE is a highly curated platform, we take the application process very seriously
-              that’s the reason we changed the way how artist can apply to be a creator on the SAGE
-              platform. Artist should present their works in a pdf format and submit it below. We
-              would like you to <span className='submissions-page__emphasis'>take your time</span>{' '}
-              to present yourself and your creations.
+              SAGE looks to unite artists and collectors together. We have designed our market to
+              emphasize curation but have added the ability to work outside this boundary. We
+              believe in balance and openness for those accepted onto the platform. We have created
+              this by balancing drops and profiles on SAGE. This empowers you to release a drop on
+              your own, without needing to coordinate again with us. Meanwhile, SAGE collectors will
+              browse the creator catalog to find your work. This creates a perfect balance between
+              (a) curation from our part and (b) the balance to support all of the top artists. In
+              balancing curation and a selection of artists, we ensure a scarcity of art on SAGE.
             </p>
           </div>
           <div className='submissions-page__guidelines-group'>
-            <h1 className='submissions-page__guidelines-header'>Standards</h1>
+            <p className='submissions-page__guidelines-header'>NFT Standards</p>
             <p className='submissions-page__guidelines-text'>
-              When it comes to digital art, we provide specific guidelines regarding technical
-              standards and image resolution. Artists’ works must meet these requirements. We do
-              this not only to ensure that the quality of the work is up to par but also to make
-              certain that the work will meet our expectations of how the featured art will look—and
-              be presented—in an exhibition outside the web. These standards are currently what we
-              look for when checking artists’ applications. In terms of animation or 3D-rendering
-              artwork, we seek a quality standard of 4k and up.
+              We provide specific guidelines regarding technical standards and image resolution
+              minted SAGE artworks once accepted on the platform. Artists must meet these
+              requirements. We do this to ensure that the quality of the work is up to par but also
+              to make certain that the work will meet our expectations of how the featured art will
+              look—and be presented—in an exhibition outside the web.
             </p>
           </div>
           <div className='submissions-page__aspect-ratios-section'>
@@ -39,26 +40,33 @@ export default function submissions() {
               <div className='submissions-page__aspect-ratios-example'>
                 <div className='submissions-page__aspect-ratios-1-1'></div>
               </div>
-              <h1 className='submissions-page__aspect-ratios-label'>1:1 artworks</h1>
+              <p className='submissions-page__aspect-ratios-label'>1:1 artworks</p>
             </div>
             <div className='submissions-page__aspect-ratios-group'>
               <div className='submissions-page__aspect-ratios-example'>
                 <div className='submissions-page__aspect-ratios-16-9'></div>
               </div>
-              <h1 className='submissions-page__aspect-ratios-label'>16:9 artworks</h1>
+              <p className='submissions-page__aspect-ratios-label'>16:9 artworks</p>
             </div>
 
             <div className='submissions-page__aspect-ratios-group'>
               <div className='submissions-page__aspect-ratios-example'>
                 <div className='submissions-page__aspect-ratios-9-16'></div>
               </div>
-              <h1 className='submissions-page__aspect-ratios-label'>9:16 artworks</h1>
+              <p className='submissions-page__aspect-ratios-label'>9:16 artworks</p>
             </div>
           </div>
           <div className='submissions-page__guidelines-group'>
-            <h1 className='submissions-page__guidelines-header'>How to Submit Your Application</h1>
+            <p className='submissions-page__guidelines-header'>How to Submit Your Application</p>
             <p className='submissions-page__guidelines-text'>
-              Once ready, please send your application to:
+              To submit your application, we require a few things:
+            </p>
+            <ul className='submissions-page__guidelines-list'>
+              <li>A brief description about yourself and your artist career.</li>
+              <li>An artist portfolio.</li>
+              <li>Links to your social media pages.</li>
+            </ul>
+            <p className='submissions-page__guidelines-text'>
               <a href='mailto:creators@sage.art'> creators@sage.art</a>
             </p>
           </div>
