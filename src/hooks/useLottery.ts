6f34fee0951f948326fd710180f7b8a93ee3d7de
Patipat <patipat@sage.art>
2022-10-27 15:00:41 -0400

    fix: game durations to round to 2 decimal places

--- src/hooks/useLottery.ts
+++ src/hooks/useLottery.ts
@@ -28,7 +28,7 @@ export default function useLottery({ lottery, nfts, selectedIndex }: UseLotteryA
   duration = duration / 1000;
   duration = duration / 60 / 60;
   const { data: lotteryWinners } = useGetWinnersQuery(lottery.id, { skip: !isEnded });
-  const durationDisplay = `${duration} hour${duration > 1 ? 's' : ''}`;
+  const durationDisplay = `${duration.toFixed(2)} hour${duration > 1 ? 's' : ''}`;
   const pixelGameInfo =
     "Users can buy tickets for this drop using only Pixel points, which are a SAGE reward for those holding the ASH token in their wallets. Once the entry period is closed, the drawing takes place and the winners, who will be able to mint this collectible, are selected using SAGE's integration with Chainlink.";
   const ashOnlyGameInfo = `Users can enter a live drawing for ${durationDisplay}. At entry, users will be asked to pay the sales price. Once the entry period is closed, the drawing takes place and SAGE selects the game winner through our RNG integration with Chainlink. Users that don't win will receive their refund automatically when gas is at 10 GWEI or below.`;
