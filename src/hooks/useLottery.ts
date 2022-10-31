b545fc37c6b3c6f4a311673379b52937a7e8bf50
Patipat <patipat@sage.art>
2022-10-31 14:13:16 -0400

    fix: points only game description

--- src/hooks/useLottery.ts
+++ src/hooks/useLottery.ts
@@ -29,15 +29,24 @@ export default function useLottery({ lottery, nfts, selectedIndex }: UseLotteryA
   duration = duration / 60 / 60;
   const { data: lotteryWinners } = useGetWinnersQuery(lottery.id, { skip: !isEnded });
   const durationDisplay = `${duration.toFixed(2)} hour${duration > 1 ? 's' : ''}`;
-  const requiresASH: boolean = !!lottery.costPerTicketTokens;
+  const requiresASH: boolean = !!lottery.costPerTicketTokens && lottery.costPerTicketPoints > 0;
   const requiresPoints: boolean = !!lottery.costPerTicketPoints;
+
   const freeGameInfo =
-    'This drawing is token gated. Only users with The Internship token in their wallet can claim one free entry. Once the entry period is closed, winners will be chosen by our integration with Chainlink and given the ability to mint the NFT.';
+    'This drawing is free to enter. Once the entry period is closed, winners will be chosen by our integration with Chainlink and given the ability to mint the NFT.';
   const pixelOnlyGameInfo =
     "Users can buy tickets for this drop using only Pixel points, which are a SAGE reward for those holding the ASH token in their wallets. Once the entry period is closed, the drawing takes place and the winners, who will be able to mint this collectible, are selected using SAGE's integration with Chainlink.";
   const defaultGameInfo = `Users can enter a live drawing for ${durationDisplay}. At entry, users will be asked to pay the sales price. Once the entry period is closed, the drawing takes place and SAGE selects the game winner through our RNG integration with Chainlink. Users that don't win will receive their refund automatically when gas is at 10 GWEI or below.`;
-  let gameInfo = Boolean(requiresPoints && !requiresASH) ? pixelOnlyGameInfo : defaultGameInfo;
-  if (!Boolean(requiresASH && !requiresPoints)) {
+
+  //ASH ONLY - informs about refunds
+  let gameInfo: string = defaultGameInfo;
+  //POINTS ONLY
+  if (Boolean(requiresPoints && !requiresASH)) {
+    gameInfo = pixelOnlyGameInfo;
+  }
+
+  //NO ASH NOR POINTS - usually offered behind tokengating
+  if (Boolean(!requiresASH && !requiresPoints)) {
     gameInfo = freeGameInfo;
   }
 
